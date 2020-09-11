0fea60fac5ce1a9b1191d3673854614c29ec176a
Ned Twigg <ned.twigg@diffplug.com>
2020-09-11 11:56:22 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+import { X, Bookmark, Share } from "react-feather";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <X />
+      <Bookmark />
+      <Share />
+    </div>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+
+module.exports = {
+  mode: "production",
+  plugins: [new BundleAnalyzerPlugin()],
+  entry: {
+    app: ["./src/App.js"]
+  },
+  output: { filename: "[name].bundle.js" },
+  resolve: { extensions: [".ts", ".tsx", ".js"] },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: [/node_modules/],
+        use: { loader: "awesome-typescript-loader" }
+      }
+    ]
+  }
+};
